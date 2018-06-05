f9ea6e7901846174e2c6181a714c5953fc2b3422
ricknussell31 <ricknussell31@gmail.com>
2018-06-05 17:49:29 -0400

    Fixed Issues with Blobs
    There was an issue with the matrix solver that was fixed since we had the wrong system of equations. If we tile a 2 by 2, the horizontal/vertical blobs will get counted 6 times and not 3. 
    
    Also, we condensed the code and fixed an issue that was causing the blob detector not to catch some aggregations. Thus, we added a new function in order to do this.

--- PlanktonSignaling/BlobDetection.py
+++ PlanktonSignaling/BlobDetection.py
@@ -8,58 +8,24 @@ import profile
 from cv2 import *
 from scipy import ndimage
 
-def constantDep(c,depMaxStr,**kwargs):
-    '''Constant deposition function'''
-    return(array(depMaxStr*ones(len(c))))
+def countingAll(thresh, maxValue, swimmers):
+    fir = countblobNew(0,thresh,maxValue,swimmers)
+    sec = countblobNew(255,thresh,maxValue,swimmers)
+    return(fir + sec);
 
-def atanDep(c,depMaxStr,depThreshold=0.08,depTransWidth=1/250,**kwargs):
-    '''arctan (soft switch) transition function'''
-    return(depMaxStr/pi*(arctan((-c+depThreshold)/depTransWidth)+pi/2))
+def countblobNew(M,thresh, maxValue, swimmers):
+    a = blobdetectNew(M,1,thresh, maxValue, swimmers)
+    b = blobdetectNew(M,2,thresh, maxValue, swimmers)
+    c = blobdetectNew(M,3,thresh, maxValue, swimmers)
+    A = np.array([[1,2,4],[4,6,9],[9,12,16]])
+    B = np.array([[a],[b],[c]])
+    x = np.linalg.solve(A,B)
+    return(x.sum());
 
-def linAtanDep(c,depMaxStr,depThreshold=0.08,depTransWidth=1/250,**kwargs):
-    '''arctan (soft switch) transition function'''
-    return(depMaxStr/pi*(c+0.1*depThreshold)/1.1/depThreshold*(arctan((-c+depThreshold)/depTransWidth)+pi/2))
-
-
-def blobdetect(thresh, maxValue, swimmers):
+def blobdetectNew(M, N, thresh, maxValue, swimmers):
     im2 = np.array(swimmers*255/0.08, dtype = np.uint8)
-    threshed = cv2.adaptiveThreshold(im2, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 0)
-    plt.imshow(im2)
-
-    th, dst = cv2.threshold(im2, thresh, maxValue, cv2.THRESH_BINARY)
-    plt.imshow(dst)
-    im = cv2.bitwise_not(dst)
-
-    params = cv2.SimpleBlobDetector_Params()
-
-    params.minThreshold = 0
-    params.maxThreshold = 300
-    params.filterByConvexity = False
-    params.filterByArea = False
-
-    detector = cv2.SimpleBlobDetector_create(params)
-
-    #Detect the blobs
-
-    keypoints = detector.detect(im)
-
-    im=cv2.bitwise_not(im)
-
-    #plot the blob contours
-
-    #im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    #plt.imshow(im_with_keypoints)
-
-    #outputs how many blobs there are 
-
-    return(len(keypoints));
-
-def blobdetect2(thresh, maxValue, swimmers):
-    im2 = np.array(swimmers*255/0.08, dtype = np.uint8)
-    im3 = np.tile(im2,(2,2))
-    threshed = cv2.adaptiveThreshold(im3, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 0)
-    #plt.imshow(im2)
+    im3 = np.tile(im2,(N,N))
+    #threshed = cv2.adaptiveThreshold(im3, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 0)
 
     th, dst = cv2.threshold(im3, thresh, maxValue, cv2.THRESH_BINARY)
     #plt.imshow(dst)
@@ -71,6 +37,7 @@ def blobdetect2(thresh, maxValue, swimmers):
     params.maxThreshold = 300
     params.filterByConvexity = False
     params.filterByArea = False
+    params.blobColor = M
 
     detector = cv2.SimpleBlobDetector_create(params)
 
@@ -78,42 +45,7 @@ def blobdetect2(thresh, maxValue, swimmers):
 
     keypoints = detector.detect(im)
 
-    im=cv2.bitwise_not(im)
-
-    #plot the blob contours
-
-    #im_with_keypoints = cv2.drawKeypoints(im, keypoints, np.array([]), (0,0,255), cv2.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
-
-    #plt.imshow(im_with_keypoints)
-
-    #outputs how many blobs there are 
-
-    return(len(keypoints));
-
-def blobdetect3(thresh, maxValue, swimmers):
-    im2 = np.array(swimmers*255/0.08, dtype = np.uint8)
-    im3 = np.tile(im2,(3,3))
-    threshed = cv2.adaptiveThreshold(im3, 255, cv2.ADAPTIVE_THRESH_MEAN_C, cv2.THRESH_BINARY, 3, 0)
-    #plt.imshow(im2)
-
-    th, dst = cv2.threshold(im3, thresh, maxValue, cv2.THRESH_BINARY)
-    #plt.imshow(dst)
-    im = cv2.bitwise_not(dst)
-
-    params = cv2.SimpleBlobDetector_Params()
-
-    params.minThreshold = 0
-    params.maxThreshold = 300
-    params.filterByConvexity = False
-    params.filterByArea = False
-
-    detector = cv2.SimpleBlobDetector_create(params)
-
-    #Detect the blobs
-
-    keypoints = detector.detect(im)
-
-    im=cv2.bitwise_not(im)
+    #im=cv2.bitwise_not(im)
 
     #plot the blob contours
 
@@ -124,9 +56,3 @@ def blobdetect3(thresh, maxValue, swimmers):
     #outputs how many blobs there are 
 
     return(len(keypoints));
-
-def countblob(a,b,c):
-    A = np.array([[1,2,4],[4,3,9],[9,12,16]])
-    B = np.array([[a],[b],[c]])
-    x = np.linalg.solve(A,B)
-    return(x.sum());
