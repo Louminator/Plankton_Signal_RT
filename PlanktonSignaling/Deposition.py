40b2965229a5fa6f7aee9c4de02054b2d85c4fc4
ricknussell31 <ricknussell31@gmail.com>
2019-12-06 10:41:10 -0500

    Added NonDimensional Code and Dep Funcs
    We have extended our model to now produce a non-dimensional version of the 2D model of our run-and-tumble. As well, we have added 2 deposition functions: atanDep2 and linAtanDep2. These now are more accurate representations due to including a tanh in our representation of the heaviside.

---
+++ PlanktonSignaling/Deposition.py
@@ -0,0 +1,27 @@
+from scipy import *
+import numpy as np
+from numpy.random import rand
+import matplotlib.pyplot as plt
+import profile
+from cv2 import *
+from scipy import ndimage
+
+def constantDep(c,depMaxStr,depThreshold=0.08,depTransWidth=1/250,**kwargs):
+    '''Constant deposition function'''
+    return(array(depMaxStr*ones(len(c))))
+
+def atanDep(c,depMaxStr,depThreshold=0.08,depTransWidth=1/250,**kwargs):
+    '''arctan (soft switch) transition function'''
+    return(depMaxStr/pi*(arctan((-c+depThreshold)/depTransWidth)+pi/2))
+
+def atanDep2(c,depMaxStr,depThreshold=0.08,depTransWidth=1/250,**kwargs):
+    '''arctan (soft switch) transition function with tanh'''
+    return(depMaxStr/2*(np.tanh((-c+depThreshold)/depTransWidth)+1))
+
+def linAtanDep(c,depMaxStr,depThreshold=0.08,depTransWidth=1/250,**kwargs):
+    '''arctan (soft switch) transition function'''
+    return(depMaxStr/pi*(c+.1*depThreshold)/(1.1*depThreshold)*(arctan((-c+depThreshold)/depTransWidth)+pi/2)**2)
+
+def linAtanDep2(c,depMaxStr,depThreshold=0.08,depTransWidth=1/250,**kwargs):
+    '''arctan (soft switch) transition function'''
+    return(depMaxStr*(c+.2*depThreshold)/(depThreshold*2)*(np.tanh((-c+depThreshold)/depTransWidth)+1))
